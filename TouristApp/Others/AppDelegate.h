9af5eaae5fd1577096cde471c9cab56938a2b5ec
King-Of-Overload <yzluxintao@163.com>
2018-07-04 14:18:40 +0800

    init proj

---
+++ TouristApp/Others/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouristApp
+//
+//  Created by Salu on 2018/7/3.
+//  Copyright © 2018年 zjut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouristAppTests/TouristAppTests.m
@@ -0,0 +1,39 @@
+//
+//  TouristAppTests.m
+//  TouristAppTests
+//
+//  Created by Salu on 2018/7/3.
+//  Copyright © 2018年 zjut. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TouristAppTests : XCTestCase
+
+@end
+
+@implementation TouristAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
